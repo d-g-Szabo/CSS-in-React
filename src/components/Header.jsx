39133ed8f2fcf5d5a5b4fde6bfb58934b9c71069
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2024-06-26 12:04:26 +0100

    Init Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+// import Nav from "./Nav";
+
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <h1>My App</h1>
+      </header>
+      {/* <Nav /> */}
+    </>
+  );
+}
